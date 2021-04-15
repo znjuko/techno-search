b417fec1b98249084ab55c35332c9614a31764d5
Андрей Лисин <foxers100@gmail.com>
2021-04-15 18:40:41 +0300

    added facade

---
+++ MapService/src/map_service/map_graph/graph/graph.h
@@ -0,0 +1,26 @@
+#ifndef TECHNO_SEARCH_STORE_MANAGER_H
+#define TECHNO_SEARCH_STORE_MANAGER_H
+
+class Graph{
+private:
+    AdjajencyTable: **Double;
+    counterArray: *size_t;
+public:
+    Graph(**double Table, *size_t Array){
+
+    };
+};
+
+class FindPath{
+private:
+    path: *size_t;
+public:
+    FindPath(**double table, size_t A, size_t B){
+
+    }
+    GivePath(*size_t array){
+
+    }
+};
+
+#endif //TECHNO_SEARCH_STORE_MANAGER_H
