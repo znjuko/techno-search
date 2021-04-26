7556092e96e673a7f6058af745750effad046916
fillinmar <mar.nv17@yandex.ru>
2021-04-26 16:07:08 +0300

    компилируемый код

---
+++ TECHNO_SEARCH/cmake-build-debug/tests/unit/src/shop_service/product_service/request_models/CMakeFiles/store_service_models.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/store_service_models.dir/main.cpp.o"
+  "CMakeFiles/store_service_models.dir/models.cpp.o"
+  "store_service_models"
+  "store_service_models.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/store_service_models.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TECHNO_SEARCH/main.cpp
@@ -0,0 +1,4 @@
+//
+// Created by fillinmar on 25.04.2021.
+//
+
