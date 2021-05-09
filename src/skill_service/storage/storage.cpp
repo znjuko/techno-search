adc91b438ad35dc9d9f3839e165a964da947e87f
Chernyh.Nikita2 <Chernyh.Nikita2@wildberries.ru>
2021-05-09 20:24:07 +0300

    storage released

--- src/skill_service/storage/storage.cpp
+++ src/skill_service/storage/storage.cpp
@@ -5,28 +5,46 @@
 #include "storage.h"
 
 CounterPopularityMetricResponse MetricStorage::ResponseGetCounterPopularityByShop(
-    const GetCountersPopularityByShopRequest &req)
-{
-    return CounterPopularityMetricResponse();
+        const GetCountersPopularityByShopRequest &req) {
+    auto q = GetCounterPopularityMetricQuery();
+    q.SetupQuery(req);
+    std::shared_ptr<CounterPopularityMetricReader> reader;
+
+    this->storage->Select(q, reader);
+
+    auto res = CounterPopularityMetricResponse();
+    res.array = reader->Get();
+    return res;
 }
 
 ShopProductPopularityMetricResponse MetricStorage::GetProductsPopularityByShop(
-    const GetProductPopularityByShopRequest &req)
-{
+        const GetProductPopularityByShopRequest &req) {
+    auto q = GetProductPopularityByShopMetricQuery();
+    q.SetupQuery(req);
+    std::shared_ptr<ShopProductsPopularityMetricReader> reader;
 
+    this->storage->Select(q, reader);
 
-    return ShopProductPopularityMetricResponse();
+    auto res = ShopProductPopularityMetricResponse();
+    res.array = reader->Get();
+    return res;
 }
 
-ProductPopularityMetricResponse MetricStorage::GetProductsTotalPopularity(const GetProductsTotalPopularityRequest &req)
-{
-    return ProductPopularityMetricResponse();
+ProductPopularityMetricResponse
+MetricStorage::GetProductsTotalPopularity(const GetProductsTotalPopularityRequest &req) {
+    auto q = GetProductsTotalPopularityMetricQuery();
+    q.SetupQuery(req);
+    std::shared_ptr<ProductPopularityMetricReader> reader;
+
+    this->storage->Select(q, reader);
+
+    auto res = ProductPopularityMetricResponse();
+    res.array = reader->Get();
+    return res;
 }
 
-MetricStorage::~MetricStorage()
-{
+MetricStorage::~MetricStorage() {
 }
 
-MetricStorage::MetricStorage(std::shared_ptr<ClickStorage> storage) : storage(storage)
-{
+MetricStorage::MetricStorage(std::shared_ptr<ClickStorage> storage) : storage(storage) {
 }
