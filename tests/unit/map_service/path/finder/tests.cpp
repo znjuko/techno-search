73a15e15fce6235b7fd99afe36b56b29be916def
Андрей Лисин <foxers100@gmail.com>
2021-05-22 21:50:39 +0300

    working tests

--- tests/unit/map_service/path/finder/tests.cpp
+++ tests/unit/map_service/path/finder/tests.cpp
@@ -2,31 +2,33 @@
 #include <iostream>
 
 #include "finder.h"
+#include <vector>
 
-const double graphmatrix = {
+const std::vector<std::vector<int>> graphmatrix = {
     {0, 1, 0, 0, 0},
     {1, 0, 3, 0, 0},
-    {0, 3, 0, 1.5, 6},
-    {0, 0, 1.5, 0, 0},
+    {0, 3, 0, 1, 6},
+    {0, 0, 1, 0, 0},
     {0, 0, 6, 0, 0},
 };
 
-const size_t counters = {10, 11, 2, 95, 5};
+const std::vector<int> counters = {10, 11, 2, 95, 5};
 
-const size_t size = 5
+const size_t size = 5;
 
 TEST(find_path, standalone1)
 {
-F = PathFinder()
-int *A = F.FindPath(10, 95, graphmatrix, counters, size);
-EXPECT_EQ(A[0], 0);
-EXPECT_EQ(A[1], 1);
-EXPECT_EQ(A[2], 2);
-EXPECT_EQ(A[3], 3);
+    PathFinder F;
+    std::vector<size_t> A = F.FindPath(10, 95, size, graphmatrix, counters);
+    EXPECT_EQ(A[0], 0);
+    EXPECT_EQ(A[1], 1);
+    EXPECT_EQ(A[2], 2);
+    EXPECT_EQ(A[3], 3);
 }
 
 TEST(find_path, standalone2)
 {
-    int *A = FindPath(11, 2, graphmatrix, counters, size);
+    PathFinder F;
+    std::vector<size_t> A = F.FindPath(11, 2, size, graphmatrix, counters);
     EXPECT_EQ(A[0], 1);
 }
