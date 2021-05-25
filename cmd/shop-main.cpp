5eddcd71ed6110bc077d1fa746ae77761558a977
fillinmar <mar.nv17@yandex.ru>
2021-05-26 01:10:18 +0300

    how to create a db in src->shop-service

--- cmd/shop-main.cpp
+++ cmd/shop-main.cpp
@@ -27,45 +27,45 @@ using namespace std;
 
 int main()
 {
-//    auto portValue = std::getenv("PORT");
-//    Port port;
-//    try
-//    {
-//        port = Port(portValue);
-//    }
-//    catch (const std::exception &e)
-//    {
-//        cout << "ERROR: " << e.what() << endl;
-//        return 0;
-//    }
-//    auto addr = Address(Ipv4::any(), port);
-//
-//    auto threadCountValue = std::getenv("THREADS");
-//    if (!threadCountValue)
-//    {
-//        cout << "ERROR: "
-//             << "empty thread number" << endl;
-//        return 0;
-//    }
-//    int threadCount = 0;
-//    try
-//    {
-//        threadCount = boost::lexical_cast<int>(threadCountValue);
-//    }
-//    catch (const std::exception &e)
-//    {
-//        cout << "ERROR: "
-//             << "wrong thread count value " << threadCountValue << endl;
-//        return 0;
-//    }
-//
-//    Rest::Router router;
+    sigset_t signals;
+    if (sigemptyset(&signals) != 0 || sigaddset(&signals, SIGTERM) != 0 || sigaddset(&signals, SIGINT) != 0 ||
+        sigaddset(&signals, SIGHUP) != 0 || pthread_sigmask(SIG_BLOCK, &signals, nullptr) != 0)
+    {
+        perror("install signal handler failed");
+        return 1;
+    }
 
-
-    Port port(7781);
+    const char* portValue = std::getenv("PORT");
+    Port port;
+    try
+    {
+        port = Port(portValue);
+    }
+    catch (const std::exception &e)
+    {
+        cout << "ERROR: " << e.what() << endl;
+        return 0;
+    }
     auto addr = Address(Ipv4::any(), port);
 
-    int thr = 2;
+    auto threadCountValue = std::getenv("THREADS");
+    if (!threadCountValue)
+    {
+        cout << "ERROR: "
+             << "empty thread number" << endl;
+        return 0;
+    }
+    int threadCount = 0;
+    try
+    {
+        threadCount = boost::lexical_cast<int>(threadCountValue);
+    }
+    catch (const std::exception &e)
+    {
+        cout << "ERROR: "
+             << "wrong thread count value " << threadCountValue << endl;
+        return 0;
+    }
 
     Rest::Router router;
 
@@ -77,7 +77,50 @@ int main()
     auto errorResponseWriter = std::make_shared<ErrorResponseWriter>();
 
     // shop service part only
-    const char *options = "host=localhost port= 5432 user=fillinmar password=1234 dbname=technosearch";
+
+    auto postgresHost = std::getenv("POSTG_HOST");
+    if (!postgresHost)
+    {
+        cout << "ERROR: "
+             << "empty postgres hostname" << endl;
+        return 0;
+    }
+//    std::string a = postgresHost;
+
+    auto postgresPort = std::getenv("POSTG_PORT");
+    if (!postgresPort)
+    {
+        cout << "ERROR: "
+             << "empty postgres port" << endl;
+        return 0;
+    }
+
+    auto postgresUser = std::getenv("POSTG_USER");
+    if (!postgresUser)
+    {
+        cout << "ERROR: "
+             << "empty postgres user" << endl;
+        return 0;
+    }
+
+    auto postgresPassword = std::getenv("POSTG_PASS");
+    if (!postgresPassword)
+    {
+        cout << "ERROR: "
+             << "empty postgres pass" << endl;
+        return 0;
+    }
+
+    auto postgresDB = std::getenv("POSTG_DB");
+    if (!postgresDB)
+    {
+        cout << "ERROR: "
+             << "empty postgres database" << endl;
+        return 0;
+    }
+
+   const char *options = "host=localhost port= 5432 user=fillinmar password=1234 dbname=technosearch";
+//    const char *options = "host=" + postgresHost + "port= " +postgresPort+ " user= " +postgresUser+ " password=" + postgresPassword + " dbname= "+postgresDB;
 
     try
     {
@@ -122,72 +165,4 @@ int main()
 
     httpEndpoint->shutdown();
     return 0;
-
-    // skill service part only
-
-    //    auto clickOpts = ClientOptions();
-    //
-    //    auto clickHost = std::getenv("CLICK_HOST");
-    //    if (!clickHost)
-    //    {
-    //        cout << "ERROR: "
-    //             << "empty clickhouse hostname" << endl;
-    //        return 0;
-    //    }
-    //    clickOpts.SetHost(clickHost);
-    //    auto clickPort = std::getenv("CLICK_PORT");
-    //    if (!clickPort)
-    //    {
-    //        cout << "ERROR: "
-    //             << "empty clickhouse port" << endl;
-    //        return 0;
-    //    }
-    //    try
-    //    {
-    //        clickOpts.SetPort(boost::lexical_cast<int>(clickPort));
-    //    }
-    //    catch (const std::exception &e)
-    //    {
-    //        cout << "ERROR: " << e.what() << endl;
-    //    }
-    //    auto clickUser = std::getenv("CLICK_USER");
-    //    if (!clickUser)
-    //    {
-    //        cout << "ERROR: "
-    //             << "empty clickhouse user" << endl;
-    //        return 0;
-    //    }
-    //    clickOpts.SetUser(clickUser);
-    //    auto clickPassword = std::getenv("CLICK_PASS");
-    //    if (!clickPassword)
-    //    {
-    //        cout << "ERROR: "
-    //             << "empty clickhouse pass" << endl;
-    //        return 0;
-    //    }
-    //    clickOpts.SetUser(clickPassword);
-    //    auto clickDB = std::getenv("CLICK_DB");
-    //    if (!clickDB)
-    //    {
-    //        cout << "ERROR: "
-    //             << "empty clickhouse database" << endl;
-    //        return 0;
-    //    }
-    //    clickOpts.SetDefaultDatabase(clickDB);
-    //
-    //    auto commonClickStorage = std::make_shared<ClickStorage>(clickOpts);
-    //    auto skillStorage = std::make_shared<MetricStorage>(commonClickStorage);
-    //    auto skillManager = std::make_shared<MetricManager>(skillStorage);
-    //    auto skillDelivery = std::make_shared<MetricService>(jsonResponseWriter, jsonRequestBodyReader,
-    //    errorResponseWriter,
-    //                                                         requestQueryReader, skillManager);
-    //    skillDelivery->SetupService(&router);
-    //    // end of skill service part
-    //
-    //    auto httpEndpoint = std::make_shared<Http::Endpoint>(addr);
-    //    httpEndpoint->setHandler(router.handler());
-    //    cout << "starting server on port " << portValue << endl;
-    //    httpEndpoint->serve();
-
-    //    return 0;
 }
