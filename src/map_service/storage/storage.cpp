f4978ab9164b0b89b935a80cb22a742fe94840b8
Андрей Лисин <foxers100@gmail.com>
2021-05-27 18:34:11 +0300

    i am trying

--- src/map_service/storage/storage.cpp
+++ src/map_service/storage/storage.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "storage.h"
+
+Storage::Storage(std::shared_ptr<mongocxx::database> database) : database(database){};
+
+std::shared_ptr<StoreModel> MapStorage::GetMapData(const int &ID)
+{
+    auto storeCollection = database->collection("store_graph");
+
+    auto selectStoreResult = storeCollection.find_one(make_document(kvp("ID", ID)));
+    if (!selectStoreResult)
+    {
+        throw StoreGraphNotFound(ID);
+    }
+
+    auto selectStoreOutput = bsoncxx::to_json(*selectStoreResult);
+    return std::make_shared<StoreModel>(selectStoreOutput);
+}
+
+std::shared_ptr<AddMapResponse> MapStorage::AddMap(std::shared_ptr<AddProductRequest> req,
+                                                   std::shared_ptr<AddProductRequest> req2,
+                                                   std::shared_ptr<AddProductRequest> req3)
+{
+    auto q = std::make_shared<AddMapQuery>();
+    q->SetupQuery(req);
+    storage->Insert(q);
+
+    auto q2 = std::make_shared<AddProductQuery>();
+    q2->SetupQueryForId(req2); //translate to postgres request
+    auto reader = std::make_shared<AddProductReader>();
+    storage->Select(q2, reader);//return Id
+
+    auto res = std::make_shared<AddProductResponse>();
+    res->array = reader->Get();
+    return res;
+}
--- src/map_service/storage/storage.h
+++ src/map_service/storage/storage.h
@@ -5,4 +5,21 @@
 #ifndef TECHNO_SEARCH_STORAGE_H
 #define TECHNO_SEARCH_STORAGE_H
 
+#include "models.h"
+
+class MapStorage
+{
+public:
+    Storage() = delete;
+
+    Storage(const Storage &str) = delete;
+
+    Storage(std::shared_ptr<mongocxx::database> database);
+
+    std::shared_ptr<StoreModel> GetMapData(const int &ID);
+
+private:
+    std::shared_ptr<mongocxx::database> database;
+};
+
 #endif //TECHNO_SEARCH_STORAGE_H
